3d4e0e75047c1bb921c48faead4547556cc6fac6
Michal <info@bitcont.net>
2015-02-07 22:19:53 +0100

    Initial mess.

--- deploy.sh
+++ deploy.sh
@@ -9,4 +9,4 @@
 
 
 
-ssh root@104.131.226.162 'git -C /var/www/dron/dronweb fetch --all; git -C /var/www/dron/dronweb checkout --force origin/master; composer self-update; composer install --working-dir /var/www/dron/dronweb'
+ssh root@104.131.226.162 'ssh-agent bash; ssh-add ~/pks/dronweb.openssh; git -C /var/www/dron/dronweb fetch --all; git -C /var/www/dron/dronweb checkout --force origin/master; composer self-update; composer install --working-dir /var/www/dron/dronweb'
